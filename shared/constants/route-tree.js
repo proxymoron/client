e42bebc698d61309adb23b4eea79d15911f06a56
Max Goodman <c@chromakode.com>
2016-11-16 17:28:23 -0800

    use NoErrorTypedAction

--- shared/constants/route-tree.js
+++ shared/constants/route-tree.js
@@ -1,24 +1,24 @@
 // @flow
-import type {TypedAction} from '../constants/types/flux'
+import type {NoErrorTypedAction} from '../constants/types/flux'
 import type {RouteDefNode, Path, PropsPath} from '../route-tree'
 
 export const setRouteDef = 'routeTree:setRouteDef'
-export type SetRouteDef = TypedAction<'routeTree:setRouteDef', {routeDef: RouteDefNode}, {}>
+export type SetRouteDef = NoErrorTypedAction<'routeTree:setRouteDef', {routeDef: RouteDefNode}>
 
 export const switchTo = 'routeTree:switchTo'
-export type SwitchTo = TypedAction<'routeTree:switchTo', {path: Path, parentPath: ?Path}, {}>
+export type SwitchTo = NoErrorTypedAction<'routeTree:switchTo', {path: Path, parentPath: ?Path}>
 
 export const navigateTo = 'routeTree:navigateTo'
-export type NavigateTo = TypedAction<'routeTree:navigateTo', {path: PropsPath, parentPath: ?Path}, {}>
+export type NavigateTo = NoErrorTypedAction<'routeTree:navigateTo', {path: PropsPath, parentPath: ?Path}>
 
 export const navigateAppend = 'routeTree:navigateAppend'
-export type NavigateAppend = TypedAction<'routeTree:navigateAppend', {path: PropsPath, parentPath: ?Path}, {}>
+export type NavigateAppend = NoErrorTypedAction<'routeTree:navigateAppend', {path: PropsPath, parentPath: ?Path}>
 
 export const navigateUp = 'routeTree:navigateUp'
-export type NavigateUp = TypedAction<'routeTree:navigateUp', null, {}>
+export type NavigateUp = NoErrorTypedAction<'routeTree:navigateUp', null>
 
 export const setRouteState = 'routeTree:setRouteState'
-export type SetRouteState = TypedAction<'routeTree:setRouteState', {path: Path, partialState: {}}, {}>
+export type SetRouteState = NoErrorTypedAction<'routeTree:setRouteState', {path: Path, partialState: {}}>
 
 export const resetRoute = 'routeTree:resetRoute'
-export type ResetRoute = TypedAction<'routeTree:resetRoute', {path: Path}, {}>
+export type ResetRoute = NoErrorTypedAction<'routeTree:resetRoute', {path: Path}>
